d74bd608ab6003c0405600c053ec4711c4f14eaa
yuki <mail@yuki.sh>
2023-02-22 11:17:45 +0800

    feat: add version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import { Plugin } from '@kokkoro/core';
 import { Service } from './service';
 
+const pkg = require('../package.json');
 const plugin = new Plugin();
 const service = new Service(plugin);
+
+plugin
+  .version(pkg.version)
 
 plugin
   .command('hello')
